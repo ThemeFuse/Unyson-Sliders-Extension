ca2843303f41fa0f755da4c7a8f66bcb41ef0d4e
moldcraft <moldcraft@email.com>
2014-12-22 16:27:34 +0200

    minor fixes in shortcode options

--- shortcodes/slider/options.php
+++ shortcodes/slider/options.php
@@ -22,13 +22,35 @@ if (!empty($choices)) {
 	);
 } else {
 	$options = array(
-		'no_sliders' => array(
+		'slider_id' => array( // make sure it exists to prevent notices when try to get ['slider_id'] somewhere in the code
+			'type' => 'hidden',
+		),
+		'no-forms' => array(
 			'type' => 'html-full',
 			'label' => false,
 			'desc' => false,
-			'html' => '<div style=""><h1 style="font-weight:100; text-align:center; margin-top:80px">'. __('No Sliders Available', 'fw') .'</h1>'.
-   '<p style="text-align:center"><i>'. __('No Sliders created yet. Please go to the <br/>Sliders page and <a href="'.admin_url('post-new.php?post_type='.fw()->extensions->get('slider')->get_post_type()).'">create a new Slider</a> </i></p></div>','fw')
-
+			'html' =>
+				'<div>'.
+				'<h1 style="font-weight:100; text-align:center; margin-top:80px">'. __('No Sliders Available', 'fw') .'</h1>'.
+				'<p style="text-align:center">'.
+				'<em>'.
+				str_replace(
+					array(
+						'{br}',
+						'{add_slider_link}'
+					),
+					array(
+						'<br/>',
+						fw_html_tag('a', array(
+							'href' => admin_url('post-new.php?post_type='. fw()->extensions->get('slider')->get_post_type()),
+							'target' => '_blank',
+						), __('create a new Slider', 'fw'))
+					),
+					__('No Sliders created yet. Please go to the {br}Sliders page and {add_slider_link}.', 'fw')
+				).
+				'</em>'.
+				'</p>'.
+				'</div>'
 		)
 	);
 }
