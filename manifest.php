a246280e09edc5989b5f50fa741a206ea583532d
moldcraft <moldcraft@email.com>
2014-12-22 15:03:57 +0200

    Require page-builder on slider install
    Via page-builder you can easy display the built sliders

--- manifest.php
+++ manifest.php
@@ -4,12 +4,13 @@ $manifest = array();
 
 $manifest['name']        = __( 'Sliders', 'fw' );
 $manifest['description'] = __( "Adds the Sliders extension to your website. You'll be able to create different built in jQuery sliders for your homepage and all the other website pages.", 'fw' );
-$manifest['version'] = '1.1.1';
+$manifest['version'] = '1.1.2';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 $manifest['requirements'] = array(
 	'extensions' => array(
-		'population-method' => array()
+		'population-method' => array(),
+		'page-builder' => array(),
 	)
 );
 $manifest['github_update'] = 'ThemeFuse/Unyson-Sliders-Extension';
