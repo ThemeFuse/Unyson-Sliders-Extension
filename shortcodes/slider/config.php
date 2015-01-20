d50c9128eeda8ce7e977635625007a974c80a568
Nicolai Slovineanu <rockalife@gmail.com>
2015-01-20 11:42:31 +0200

    Update config.php

--- shortcodes/slider/config.php
+++ shortcodes/slider/config.php
@@ -6,6 +6,6 @@ $cfg = array();
 
 $cfg['page_builder'] = array(
 	'title'       => __( 'Slider', 'fw' ),
-	'description' => __( 'Slider', 'fw' ),
+	'description' => __( 'Add a Slider', 'fw' ),
 	'tab'         => __( 'Media Elements', 'fw' ),
 );
