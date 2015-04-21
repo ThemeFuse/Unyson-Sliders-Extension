5dea29a39bc2da49c6fc8c8e46727eb607826d67
Alexei Gogu <llemurya@gmail.com>
2015-04-21 13:20:26 +0300

    owl-carousel fix ThemeFuse/Unyson#475

--- extensions/owl-carousel/views/owl-carousel.php
+++ extensions/owl-carousel/views/owl-carousel.php
@@ -15,8 +15,8 @@
 
 	<div class="owl-carousel owl-theme">
 		<?php foreach ($data['slides'] as $slide): ?>
-			<?php if ($slide['multimedia_type'] === 'video') : ?>
-				<div class="item-video" style="height:<?php echo $dimensions['height'];?>px; width:<?php echo $dimensions['width'];?>px;"><a class="owl-video" href="<?php echo $slide['src'] ?>"></a></div>
+			<?php if ($slide['multimedia_type'] === 'video' && false !== wp_oembed_get($slide['src'])) : ?>
+				<div class="item-video" style="height:<?php echo $dimensions['height'];?>px;"><a class="owl-video" href="<?php echo $slide['src'] ?>"></a></div>
 			<?php endif; ?>
 
 		<?php endforeach; ?>
