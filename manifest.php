fd2a0cda40497790b5a96f986e1a248f897f4ee4
Alexei Gogu <llemurya@gmail.com>
2015-04-27 13:14:46 +0300

    change manifest version

--- manifest.php
+++ manifest.php
@@ -4,7 +4,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Sliders', 'fw' );
 $manifest['description'] = __( "Adds the Sliders extension to your website. You'll be able to create different built in jQuery sliders for your homepage and all the other website pages.", 'fw' );
-$manifest['version'] = '1.1.7';
+$manifest['version'] = '1.1.8';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 $manifest['requirements'] = array(
