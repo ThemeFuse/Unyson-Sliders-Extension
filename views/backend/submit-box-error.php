ffa8b41f99ad1280228ed28d6cc134b6e3953b09
Alexei Gogu <llemurya@gmail.com>
2015-06-19 14:57:50 +0300

    Fixed ThemeFuse/Unyson#650

---
+++ views/backend/submit-box-error.php
@@ -0,0 +1,18 @@
+<?php if (!defined('FW')) die('Forbidden'); ?>
+<div class="submitbox" id="submitpost">
+	<div id="major-publishing-actions">
+		<div id="delete-action">
+			<?php
+			if (current_user_can("delete_post", $post->ID)) {
+				if (!EMPTY_TRASH_DAYS)
+					$delete_text = __('Delete Permanently', 'fw');
+				else
+					$delete_text = __('Move to Trash', 'fw');
+				?>
+				<a class="submitdelete deletion"
+				   href="<?php echo get_delete_post_link($post->ID); ?>"><?php echo $delete_text; ?></a><?php
+			} ?>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
