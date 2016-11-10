eac0458a7ff6491feae8c018dfe105cb226a0ba9
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2016-11-10 19:01:16 +0200

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -5,6 +5,10 @@ $manifest = array();
 $manifest['name']        = __( 'Sliders', 'fw' );
 $manifest['description'] = __( "Adds the Sliders extension to your website. You'll be able to create different built in jQuery sliders for your homepage and all the other website pages.", 'fw' );
 $manifest['version'] = '1.1.15';
+$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Sliders-Extension';
+$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/slider/index.html#content';
+$manifest['author'] = 'ThemeFuse';
+$manifest['author_uri'] = 'http://themefuse.com/';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 $manifest['requirements'] = array(
@@ -12,4 +16,5 @@ $manifest['requirements'] = array(
 		'population-method' => array(),
 	)
 );
+
 $manifest['github_update'] = 'ThemeFuse/Unyson-Sliders-Extension';
