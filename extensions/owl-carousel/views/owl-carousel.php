71ce5c51f11c01af2a447a516c61477f19c0e019
moldcraft <moldcraft@email.com>
2016-11-22 13:55:43 +0200

    fixed request on each render

--- extensions/owl-carousel/views/owl-carousel.php
+++ extensions/owl-carousel/views/owl-carousel.php
@@ -1,6 +1,29 @@
 <?php if (!defined('FW')) die('Forbidden'); ?>
 
 <?php if (isset($data['slides'])): ?>
+
+	<div class="owl-carousel owl-theme">
+		<?php foreach ($data['slides'] as $slide): ?>
+			<?php if ($slide['multimedia_type'] === 'video'): ?>
+				<?php
+				if (false === ($oembed = get_site_transient($transient = 'fw:oembed:'. md5($slide['src'])))) {
+					// Cache wp_oembed_get() to prevent request on every render
+					set_site_transient(
+						$transient,
+						// do strlen() because wp_oembed_get() can be false and it will mess the above `if`
+						$oembed = strlen(wp_oembed_get($slide['src'])),
+						MINUTE_IN_SECONDS
+					);
+				}
+				?>
+				<?php if ($oembed): ?>
+				<div class="item-video" style="height:<?php echo esc_attr($dimensions['height']); ?>px;">
+					<a class="owl-video" href="<?php echo esc_attr($slide['src']); ?>"></a>
+				</div>
+				<?php endif; ?>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	</div>
 	<script type="text/javascript">
 		jQuery(document).ready(function ($) {
 			$('.owl-carousel').owlCarousel({
@@ -9,17 +32,8 @@
 				margin: 10,
 				video: true,
 				center: true
-			})
+			});
 		});
 	</script>
-
-	<div class="owl-carousel owl-theme">
-		<?php foreach ($data['slides'] as $slide): ?>
-			<?php if ($slide['multimedia_type'] === 'video' && false !== wp_oembed_get($slide['src'])) : ?>
-				<div class="item-video" style="height:<?php echo esc_attr($dimensions['height']); ?>px;"><a class="owl-video" href="<?php echo esc_attr($slide['src']); ?>"></a></div>
-			<?php endif; ?>
-
-		<?php endforeach; ?>
-	</div>
 
 <?php endif; ?>
